2a48c3612bf1c6c386ca0143ac75c8cdc1f353bb
Letam Bossman Barinua <letambarinua@gmail.com>
2023-04-13 13:13:32 +0100

    Add requestHandler.ts file which handles all routing requests

---
+++ src/requestHandler.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import http from "http";
+import { log } from "console";
+import setRoutes from "./setRoutes";
+import sendHtmlFile from "./sendHtmlFile";
+
+export default function requestHandler(
+  req: http.IncomingMessage,
+  res: http.ServerResponse<http.IncomingMessage> & {
+    req: http.IncomingMessage;
+  }
+) {
+  log({ req: req.method, res: req.url });
+
+  // Sets Header to HTML content type.
+  res.setHeader("Content-Type", "text/html");
+
+  // Sets status code for unknown routes
+  res.statusCode = 404;
+
+  // Sets 404 page for unknown routes
+  let fileName = "404.html";
+
+  // Sends HTML files for known routes
+  const setFilePath = (file: string) => (fileName = file);
+
+  // Sends HTML file depending on visited route.
+  setRoutes(req, setFilePath, res);
+
+  // Reads HTML file in views directory
+  fs.readFile(`./src/views/${fileName}`, (err, data) =>
+    sendHtmlFile(data, err, res)
+  );
+}
